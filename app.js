0858148e400f30dc84ec1e86b28e489b5b597da8
SamarpitSantoki <samarpit.santoki@gmail.com>
2021-11-12 16:06:05 +0530

    added some product cards

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const dotenv = require('dotenv');
 const express = require ('express');
 const mongoose = require('mongoose');
 const app = express();
-
 dotenv.config({ path:'./.env' });
+const PORT = parseInt(process.env.PORT) || 3000;
+
 
 //link router file
 app.use(require('./router/auth'));
 app.use(express.static(__dirname));
-const PORT = process.env.PORT || 3000;
 
 // listen to PORT
 app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`));
