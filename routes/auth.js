5a1d26e8e3fa23e191daa196c3ee9cf2e0d5e2bf
SamarpitSantoki <samarpit.santoki@gmail.com>
2021-11-17 09:58:32 +0530

    Finale SemiFinale

--- routes/auth.js
+++ routes/auth.js
@@ -15,76 +15,70 @@ router.use(bodyParser.urlencoded({
 }))
 
 router.use(session({
-    name : SESS_NAME,
-    resave : false,
-    saveUninitialized : false,
-    secret : SESS_SECRET,
-    cookie :{
-        maxAge : 1000*60*60,
-        sameSite : true,
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        sameSite: true,
     }
 }))
 
-const redirectLogin = (req,res,next) => {
-    if (!req.session.userId){
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
         res.redirect('/login')
-    }else{
+    } else {
         next()
     }
 }
 
 // function for redirecting to home if user logged in
-const redirectHome = (req,res,next) => {
-    if (req.session.userId){
+const redirectHome = (req, res, next) => {
+    if (req.session.userId) {
         res.redirect('/')
-    }else{
+    } else {
         next()
     }
 }
 
 // Routes
-router.get('/signup',redirectHome, (req,res)=>{
+router.get('/signup', redirectHome, (req, res) => {
     res.render('signup');
 })
 
-router.get('/login',redirectHome, (req,res)=>{
+router.get('/login', redirectHome, (req, res) => {
     res.render('login');
 })
 
-router.post('/signup', async (req,res)=>{
-   let active_ids = await User.countDocuments({}) + 1;
-   const { name, email , password } = req.body;
-   //check all fields are filled
-   if(name && email && password){
-       // to check if user already exists
-       const exists = User.findOne({email:email});
-       if(!exists){  
-           const user = new User({
-               id : active_ids++,
-               name,
-               email,
-               password
-           });
+router.post('/signup', async (req, res) => {
+    let active_ids = await User.countDocuments({}) + 1;
+    const { name, email, password } = req.body;
+    //check all fields are filled
+    if (name && email && password) {
+        // to check if user already exists
+        const exists = User.findOne({ email: email });
+        if (exists) {
+            const user = new User({
+                id: active_ids++,
+                name,
+                email,
+                password
+            });
 
-        //    bcrypt.genSalt(10, (err,salt)=>{
-        //     bcrypt.hash(user.password, salt,(err,hash)=>{
-        //         if(err){console.log(err);}
-        //         user.password= hash;
-//     });
-//    });
-                user.save().then(() => console.log("User Registered.")).catch(()=>res.redirect('/signup'));
-           req.session.userId = user.id;
-           console.log(req.session);
-           return res.redirect('/');
-       }
-   }
-   res.redirect('/signup');
+            user.save().then(() => console.log("User Registered.")).catch(() => res.redirect('/signup'));
+            req.session.userId = user.id;
+            console.log(req.session);
+            return res.redirect('/');
+        }
+    }
+    res.redirect('/signup');
 })
 
-router.post('/login', async (req,res)=>{
-    const { email , password } = req.body;
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
-    if(email && password ) {
+    if (email && password) {
         // authentication
         // bcrypt.genSalt(10, (err,salt)=>{
         //     bcrypt.hash(password, salt,(err,hash)=>{
@@ -92,17 +86,17 @@ router.post('/login', async (req,res)=>{
         //         user.password= hash;
         //     });});
 
-        const user = await User.findOne({ email:email,password:password });
+        const user = await User.findOne({ email: email, password: password });
         if (user) {
             req.session.userId = user.id;
             return res.redirect('/');
         }
-    }res.redirect('/login');
+    } res.redirect('/login');
 })
 
-router.post('/logout',redirectLogin, (req,res)=>{
+router.post('/logout', redirectLogin, (req, res) => {
     req.session.destroy(err => {
-        if(err){
+        if (err) {
             res.redirect('/');
         }
         res.clearCookie(SESS_NAME)
